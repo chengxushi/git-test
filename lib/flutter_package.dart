0e704cce5a7d985783d7b03dfe35867bc518394a
yiFang <a12162266@163.com>
2021-07-14 15:11:31 +0800

    功能分支test_five第五次提交

--- lib/flutter_package.dart
+++ lib/flutter_package.dart
@@ -22,6 +22,7 @@ class Calculator {
 
 //master 第三轮测试
 //24 test_five 第一次提交 / 第四次中修改了
-//25 test_five 第二次提交
 //26 test_five 第三次提交 / 第四次中修改了
 //27 test_five 第四次提交 并修改了 第一次与第三次中的文件
+
+//28 test_five 第五次提交 并修改了 26行 并删除了第二次中的
