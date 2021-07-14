79bd1cad6c3efcab3f41ecef06eeb4c5395c4ead
yiFang <a12162266@163.com>
2021-07-14 15:03:01 +0800

    合并test_five分支
    功能分支test_five第二次提交(合并1)
    
    功能分支test_five第三次提交-合并2
    
    功能分支test_five第四次提交-合并3
    
    功能分支test_five第五次提交-合并4
    
    功能分支test_five第五次提交

--- lib/flutter_package.dart
+++ lib/flutter_package.dart
@@ -19,3 +19,10 @@ class Calculator {
 //18 test_three分支第2次修改
 
 //19 test_four第二次修改
+
+//master 第三轮测试
+//24 test_five 第一次提交 / 第四次中修改了
+//26 test_five 第三次提交 / 第四次中修改了
+//27 test_five 第四次提交 并修改了 第一次与第三次中的文件
+
+//28 test_five 第五次提交 并修改了 26行 并删除了第二次中的
